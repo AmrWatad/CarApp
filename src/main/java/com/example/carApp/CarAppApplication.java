fd59512c1d594d12404c3e233ad3c37c27640873
amr <amr.wtad@gmail.com>
2019-01-08 20:48:42 +0200

    Hero

--- src/main/java/com/example/carApp/CarAppApplication.java
+++ src/main/java/com/example/carApp/CarAppApplication.java
@@ -9,9 +9,7 @@ import com.example.carApp.domain.CarRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Bean;
 
 import com.example.carApp.domain.Car;
@@ -19,12 +17,12 @@ import com.example.carApp.domain.Car;
 @SpringBootApplication
 public class CarAppApplication {
 	  private static final Logger logger = LoggerFactory.getLogger(CarAppApplication.class);
-	  @Autowired // bbkasha 
+	   // bbkasha 
 	  /*We have to inject our car repository into the main class into be able to save new car objects to the database. 
 An @Autowired annotation is used to enable dependency injection. 
 The dependency injection allows us to pass dependencies into a object. 
 After we have injected the repository class, we can use the CRUD methods it provides.
-*/
+*/	@Autowired
       private CarRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(CarAppApplication.class, args);
@@ -34,7 +32,7 @@ After we have injected the repository class, we can use the CRUD methods it prov
 	@Bean /// the magic creat a resposetory thats include all tha data acces that we write to data base;
     CommandLineRunner runner(){
       return args -> {
-        // Save demo data to database
+//        // Save demo data to database
         repository.save(new Car("Ford", "Mustang", "Red",
          "ADF-1121", 2017, 59000));
         repository.save(new Car("Nissan", "Leaf", "White",
